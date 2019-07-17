346989fb790aac1289ab5903173b40699833b0f5
Stephen Griffith <stgriffi@microsoft.com>
2019-07-17 10:29:48 -0400

    Orgnized and sorted using statements

--- proxyfunc.cs
+++ proxyfunc.cs
@@ -1,15 +1,13 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace RESTProxy
 {
